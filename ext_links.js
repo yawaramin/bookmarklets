4cc7b881ba4906f47d84e931ab19b9e78831ea3c
Yawar Amin <yawar.amin@gmail.com>
2024-01-02 11:24:33 -0500

    Adjust ext links styling

--- ext_links.js
+++ ext_links.js
@@ -3,7 +3,14 @@ javascript:(() => {
   const extCls = 'github-yawaramin-ext-link';
   const extSty = document.createElement('style');
 
-  extSty.innerText = `.${extCls} {background-color:#e9e9e9; border-radius:10px; margin-left:4px; padding:4px; font-size:small;}`;
+  extSty.innerText = `.${extCls} {
+  background-color:#e9e9e9;
+  border-radius:10px;
+  margin-left:4px;
+  padding:4px;
+  font-family: sans-serif;
+  font-size:x-small;
+}`;
   document.head.appendChild(extSty);
 
   document.querySelectorAll('a').forEach(a => {
